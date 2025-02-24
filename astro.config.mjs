56d4b4446bb7c047f2a3b839661bfe8f12cf3b6d
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2025-02-24 13:57:35 +0200

    small optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import stylex from 'astro-stylex';
-
 import sitemap from '@astrojs/sitemap';
-
 import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
