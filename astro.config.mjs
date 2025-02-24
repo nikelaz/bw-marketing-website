f726e7b6244c3c885236ddad67136f11dafe7859
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2025-02-24 13:56:25 +0200

    small adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,16 +6,14 @@ import stylex from 'astro-stylex';
 
 import sitemap from '@astrojs/sitemap';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.budgetwarden.com',
-  integrations: [
-    react(),
-    stylex(),
-    sitemap({
-      filter: (page) => page !== 'https://www.budgetwarden.com/admin/',
-    })
-  ],
+  integrations: [react(), stylex(), partytown(), sitemap({
+    filter: (page) => page !== 'https://www.budgetwarden.com/admin/',
+  })],
   prefetch: true,
   experimental: {
     svg: true,
